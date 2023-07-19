5f1a3cc11aa8e7f06e926fa0cbb27ce8ecd2582c
Chris Davis <chris.davis4@gmail.com>
2023-07-19 15:40:24 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Cardlist from './Cardlist';
+import reportWebVitals from './reportWebVitals';
+import 'tachyons'
+import {robots} from './robots'
+
+ReactDOM.render(
+    <Cardlist robots ={robots}/>
+    ,document.getElementById('root'))
+reportWebVitals();
