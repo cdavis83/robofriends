f31f9fc7d4ff7b31531f4a24e68d9a84a2b0a0a5
Chris Davis <chris.davis4@gmail.com>
2023-08-10 11:03:24 -0500

    updating repo

--- src/components/ErrorBoundry.js
+++ src/components/ErrorBoundry.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class ErrorBoundry extends React.Component {
+class ErrorBoundry extends Component {
     constructor(props) {
         super(props);
         this.state = {
